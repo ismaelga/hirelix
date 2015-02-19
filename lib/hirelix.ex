4c88b369381369943dbbd4ba4856906d9c3704de
Ismael Abreu <ismaelga@users.noreply.github.com>
2015-02-19 03:13:15 +0000

    Remove unsupported start arguments

--- lib/hirelix.ex
+++ lib/hirelix.ex
@@ -11,15 +11,14 @@ defmodule Hirelix do
     end
   end
 
-  def start(host \\ "127.0.0.1", port \\ 6379, database \\ 0,
-            password \\ "", reconnect_sleep \\ :no_reconnect), do:
-    start_link(host, port, database, password, reconnect_sleep)
+  def start(host \\ "127.0.0.1", port \\ 6379), do:
+    start_link(host, port)
     |> elem 1
 
   @type reconnect_sleep :: :no_reconnect | integer
   @type start_link      :: { :ok, pid } | { :error, term }
 
-  def start_link(host \\ "127.0.0.1", port \\ 6379, database \\ 0, password \\ "", reconnect_sleep \\ :no_reconnect) do
+  def start_link(host \\ "127.0.0.1", port \\ 6379) do
     :hierdis.connect(String.to_char_list(host), port)
   end
 
